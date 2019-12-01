7499ed205e7fc784cc24f75bb97e2c0c628435c1
iamspl <shivaprasad.l@outlook.com>
2019-11-30 18:22:22 -0600

    initial commit

---
+++ src/Prime.java
@@ -0,0 +1,22 @@
+public class Prime {
+
+    public static void main(String Args[]){
+
+        System.out.println(" The largest Prime Factor of a given number is" + getLargestPrime(43));
+    }
+
+    public static int getLargestPrime(int number) {
+        int i;
+        if(number <2){
+            return -1;
+        }
+        for (i = 2; i <= number; i++) {
+            if (number % i == 0) {
+                number /= i;
+                i--;
+            }
+        }
+
+        return i;
+    }
+}
---
+++ src/scan.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class scan {
+    public static void main(String Args[]){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your Name:");
+        String name = scanner.nextLine();
+        System.out.println(" your name is " + name);
+    }
+}
