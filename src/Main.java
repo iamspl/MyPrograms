b8404ce3a61d8676c56b5f2292a4ea74808ac309
iamspl <shivaprasad.l@outlook.com>
2019-12-07 14:13:40 -0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main (String Args[]){
+
+        Account shivaAccount = new Account();
+        shivaAccount.withdrawal(100.0);
+        shivaAccount.deposit(50.0);
+        shivaAccount.withdrawal(100);
+
+
+    }
+}
